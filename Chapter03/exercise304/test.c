d3e66460dd4e072c903bbf545507f35275e2fdf7
tehmeerali786 <freelanceali786@gmail.com>
2022-02-14 08:03:56 -0600

    Chapter 03 1

---
+++ Chapter03/exercise304/test.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+int math() 
+
+{
+    int i;
+    for (i=1; i<=0; i++)
+
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+
+
+}
---
+++ Chapter03/exercise307/build.sh
@@ -0,0 +1,10 @@
+set -ex
+
+USER=tehmeerali
+SERVICENAME=basic-app 
+
+version=`cat VERSION`
+echo "version: $version"
+
+
+docker build -t $USER/$SERVICENAME:$version .
